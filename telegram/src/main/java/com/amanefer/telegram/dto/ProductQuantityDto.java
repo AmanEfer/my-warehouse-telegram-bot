55f33e0fc7ecf3b1e9178238476739023e0e7360
Alex Pripisnov <a.filtsov@gmail.com>
2024-04-29 15:38:14 +0300

    Storage-1: Create functionality for 'stock' - enabled flyway in crud pom

--- telegram/src/main/java/com/amanefer/telegram/dto/ProductQuantityDto.java
+++ telegram/src/main/java/com/amanefer/telegram/dto/ProductQuantityDto.java
@@ -1,10 +1,14 @@
 package com.amanefer.telegram.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductQuantityDto {
 
     private StockDto stock;
